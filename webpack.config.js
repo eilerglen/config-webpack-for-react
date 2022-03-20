14eee08b2401a2e90bab0b0cc65f50f438f246ce
eilerglen <83466883+eilerglen@users.noreply.github.com>
2022-03-21 01:34:03 +0300

    Webpack-config-for-react-app

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.join(__dirname, './dist'),
+    compress: true,
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+}
